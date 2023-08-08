c27e80eed439a6a57e7256384673194e1ef7eaa7
MathieuDeveloper <topfermathieu@yahoo.fr>
2023-08-08 23:45:09 +0200

    State Management Overview

---
+++ Shared/Idea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeniusChef.Shared
+{
+    public class Idea
+    {
+        //on utilise pas Guid car OpenIA ne génère pas de Guid mais un int
+        public int index { get; set; }
+        public string? title { get; set; }
+        public string? description { get; set; }
+
+    }
+}
