ff1a8afe348f30a95afc80fc731eb871cee28bbd
MathieuDeveloper <topfermathieu@yahoo.fr>
2024-03-06 23:54:17 +0100

    create SampleData

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,19 @@
+﻿using GeniusChef.Server.Data;
+using GeniusChef.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeniusChef.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpPost, Route("GetRecipeIdeas")]
+        public async Task<ActionResult<List<Idea>>> GetRecipeIdeas()
+        {
+            return SampleData.RecipeIdeas;
+        }
+
+    }
+}
